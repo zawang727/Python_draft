886478013a9ac9b58e5a92f4f8609954e0c20b29
Jennan Wang <44350375+zawang727@users.noreply.github.com>
2021-10-01 14:04:37 +0800

    Add files via upload

--- 3D_magnetic_electron_spectrommeter.py
+++ 3D_magnetic_electron_spectrommeter.py
@@ -29,6 +29,9 @@ B_strength = 0.22 #Tesla
 Is_magnetic_homo = True
 MagneticField2D = []
 MagneticFilePath = 'magnet_1_measurement_for_test.csv'
+gp0 = 0.;
+ggamma0 = 0.;
+gv0 = 0;
 
 Is2D = True
 # Location 'noFieldRegion', 'FieldRegion', 'Spectrometer', 'Outboder', 'EnergyTooLow'
@@ -39,9 +42,13 @@ m0 = 9.1*pow(10,-31)
 q = 1.6*pow(10,-19)
 
 class particle_state():
-    def __init__(self):
-        self.cor=np.array([0.,0.,0.], dtype = float)
-        self.v=np.array([0.,0.,0.], dtype = float)
+    def __init__(self,cornum = None, vnum = None):
+        if vnum is None:
+            self.cor=np.array([0.,0.,0.], dtype = float)
+            self.v=np.array([0.,0.,0.], dtype = float)
+        else:
+            self.cor=cornum
+            self.v=vnum
 
 def IsInMagBox(coordinate):
     if (coordinate[0]<boxgeo[0] or coordinate[0]>boxgeo[1]): return False
@@ -62,12 +69,15 @@ def IsOutBoder(coordinate):
     if (coordinate[2]<-0.5*LZ or coordinate[2]>0.5*LZ): return True
     return False
 
-def gamma(velocity):
+def gamma_vec(velocity):
     magnitude_square = velocity[0]*velocity[0]+velocity[1]*velocity[1]+velocity[2]*velocity[2]
     return 1./math.sqrt(1.-(magnitude_square/c2))
 
+def gamma_pure(velocity):
+    return 1./math.sqrt(1.-(velocity*velocity/c2))
+
 def momentum(velocity):
-    g = gamma(velocity)
+    g = gamma_vec(velocity)
     gm0 = g*m0
     return np.array([gm0*velocity[0],gm0*velocity[1],gm0*velocity[2]], dtype = float)
 
@@ -89,21 +99,20 @@ def updateVelocity(state,delta_t):
     p_new= momentum(state.v)+ dpdt*dt;
     p_magnitude =math.sqrt( p_new[0]*p_new[0]+p_new[1]*p_new[1]+p_new[2]*p_new[2])
     v_magnitude =math.sqrt( state.v[0]*state.v[0]+state.v[1]*state.v[1]+state.v[2]*state.v[2])
-    g=p_magnitude/(v_magnitude*m0)
+    g=p_magnitude/(v_magnitude*m0)  
     state.v[0]= p_new[0]/(g*m0)
     state.v[1]= p_new[1]/(g*m0)
     state.v[2]= p_new[2]/(g*m0)
     
 def updateVelocity2(state,delta_t):
     B = updateMagneticField(state.cor,B_strength)
-    g = gamma(state.v)
+    g = gamma_vec(state.v)
     state.v[0] = state.v[0] + q/g/m0*state.v[1]*delta_t*B
     state.v[1] = state.v[1] - q/g/m0*state.v[0]*delta_t*B  
-    
+
     
 def calcElectronVel(energy_in_MeV):
     mc2 = 0.511 #Mev/c^2
-    #return math.sqrt(c2-c2/math.pow((1+energy_in_MeV/mc2),2))
     return math.sqrt(1-1/math.pow((1+energy_in_MeV/mc2),2))*2.9979*pow(10,8);
     
 def source(coordinate, energy_in_MeV, thetaY = 0., thetaZ = 0.):
@@ -112,6 +121,8 @@ def source(coordinate, energy_in_MeV, thetaY = 0., thetaZ = 0.):
     state.v[0] =  calcElectronVel(energy_in_MeV)*math.cos(thetaY)*math.cos(thetaZ)
     state.v[1] =  calcElectronVel(energy_in_MeV)*math.sin(thetaY)*math.cos(thetaZ)
     state.v[2] =  calcElectronVel(energy_in_MeV)*math.cos(thetaY)*math.sin(thetaZ)
+    global ggamma0 
+    ggamma0 = gamma_vec(state.v)
     return state
 
 def check_pos(state: particle_state):
@@ -135,17 +146,38 @@ def updateLocation(state,delta_t):
     state.cor[2]+=state.v[2]*delta_t
     return check_pos(state)
 
+def updateLocation4thRK(state,delta_t):
+    global ggamma0
+    k1x = delta_t * (state.v)
+    k1p = delta_t * (q * np.cross(state.v, np.array([0.,0.,updateMagneticField(state.cor,B_strength)], dtype = float)))
+    vtmp = state.v + k1p/2/ggamma0/m0
+    xtmp = state.cor + k1x/2
+    k2x = delta_t * (vtmp)
+    k2p = delta_t * (q * np.cross(vtmp, np.array([0.,0.,updateMagneticField(xtmp,B_strength)], dtype = float)))
+    vtmp = state.v + k2p/2/ggamma0/m0
+    xtmp = state.cor + k2x/2
+    k3x = delta_t * (vtmp)
+    k3p = delta_t * (q * np.cross(vtmp, np.array([0.,0.,updateMagneticField(xtmp,B_strength)], dtype = float)))
+    vtmp = state.v + k3p/ggamma0/m0
+    xtmp = state.cor + k3x
+    k4x = delta_t * (vtmp)
+    k4p = delta_t * (q * np.cross(vtmp, np.array([0.,0.,updateMagneticField(xtmp,B_strength)], dtype = float)))
+    state.cor = state.cor + k1x/6 + k2x/3 + k3x/3 + k4x/6
+    state.v = state.v + (k1p/6 + k2p/3 + k3p/3 + k4p/6)/ggamma0/m0
+    return check_pos(state)
+
 def aElectronPathCalc(state,delta_t):
     pathrecord = []
     stepcounter = 0
     while(True):
         pathrecord.append(state.cor.copy())
         stepcounter += 1
-        flagLocation = updateLocation(state,delta_t)
+        flagLocation = check_pos(state);
         if (flagLocation==0):
+            updateLocation(state,delta_t)
             continue
         elif (flagLocation==1):
-            updateVelocity2(state,delta_t)
+            updateLocation4thRK(state,delta_t)
         elif (flagLocation==2):
             print('To Spectrom and finish')
             print(state.cor[0],state.cor[1],state.cor[2])
